9596f5ec0a3c70cc2fa0596d7709571c119a3dd6
Gianluca Serao <gianluca@MBP-di-Gianluca.lan>
2020-06-12 15:00:24 +0200

    main
    Co-authored-by: matildao-pane <edlitam@hotmail.it>
    Co-authored-by: thorongil05 <officialantonio@outlook.it>
    Co-authored-by: ragnar1002 <lorenzobarigliano@gmail.com>

--- k-means/src/main/java/it/unipi/hadoop/KMeans.java
+++ k-means/src/main/java/it/unipi/hadoop/KMeans.java
@@ -1,13 +1,18 @@
 package it.unipi.hadoop;
 
 import java.io.IOException;
+import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.SequenceFileInputFormat;
+import org.apache.hadoop.io.SequenceFile.Reader;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -65,8 +70,6 @@ public class KMeans {
 
     }
 
-
-
     public static class KMeansCombiner extends Reducer<IntWritable, Point, IntWritable, Point> {
     
         public void reduce(IntWritable centroid, Iterable<Point> points, Context context) 
@@ -81,27 +84,9 @@ public class KMeans {
             }
 
             sum.setNumberOfPoints(numPoints);
-            context.write(centroid, sum);  
+            context.write(centroid, sum);
         }
     }
-
-/*
- Reducer
-**Input:** (Centroid_id, List of partial_sums)
-
-**Reduce function**
-```
-number_of_point = 0
-sum = 0
-for each point in list of points:
-    sum += point
-    number_of_points += 1
-
-centroid_new_value = sum / number_of_points
-```
-
-**Output:** (Centroid_id, centroid_new_value)*/
-
 
     public static class KMeansReducer extends Reducer<IntWritable, Point, IntWritable, Point >{
 
@@ -124,10 +109,27 @@ centroid_new_value = sum / number_of_points
             // TODO: save shared centroid
 
             context.write(centroid, newCentroidValue);
-
+             
         }
     }
 
+    private static boolean stoppingCriterion(Point[] oldCentroids, Point[] newCentroids, int distance, float treshold) {
+        boolean check = true;
+
+        for(int i = 0; i < oldCentroids.length; i++) {
+            check = oldCentroids[i].distance(newCentroids[i], distance) < treshold;
+
+            if (!check) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Point[] centroidsInit(URI uri, Configuration conf, String filename, int dim, int k) {
+        
+    } 
 
     
     public static void main(String[] args) throws Exception {
@@ -135,37 +137,59 @@ centroid_new_value = sum / number_of_points
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length != 4) {
-            System.err.println("Usage: K-Means <number_of_clusters> <threshold> <input> <output>");
+            System.err.println("Usage: KMeans <input> <output>");
             System.exit(1);
         }
-        System.out.println("args[0]: number_of_cluster="  + otherArgs[0]);
-        System.out.println("args[1]: threshold=" + otherArgs[1]);
-        System.out.println("args[2]: <input>="  + otherArgs[2]);
-        System.out.println("args[3]: <output>=" + otherArgs[3]);
-
-        // TODO: mapreduce stages (while)
-        // TODO: initial centroid selections
-
-        Job job = Job.getInstance(conf, "KMeans");
-        job.setJarByClass(KMeans.class);
-        job.setMapperClass(KMeansMapper.class);
-        job.setCombinerClass(KMeansCombiner.class);
-        job.setReducerClass(KMeansReducer.class);
         
-        //one task each centroid
-        job.setNumReduceTasks(Integer.parseInt(otherArgs[0]));
+        System.out.println("args[0]: input="  + otherArgs[0]);
+        System.out.println("args[1]: output=" + otherArgs[1]);
 
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(Point.class);
+        //settings, then by user
+        int d = 3; // point dim  3 or 7
+        int k = 7; // #centroids 7 or 13
+        int dist = 2; //distance
+        float t = 0.0001f; //treshold
 
-        FileInputFormat.addInputPath(job, new Path(otherArgs[2]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[3]));
+        Point[] oldCentroids = new Point[k];
+        Point[] newCentroids = new Point[k];
 
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+        newCentroids = centroidsInit(d, k);
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        boolean stop = false;
+        int i = 0;
+        while(!stop) {
+            i++;
+            Job job = Job.getInstance(conf, "iteration " + i);
+            job.setJarByClass(KMeans.class);
+            job.setMapperClass(KMeansMapper.class);
+            job.setCombinerClass(KMeansCombiner.class);
+            job.setReducerClass(KMeansReducer.class);
+            
+            //one task each centroid
+            job.setNumReduceTasks(Integer.parseInt(otherArgs[0]));
 
+            job.setOutputKeyClass(IntWritable.class);
+            job.setOutputValueClass(Point.class);
+
+            FileInputFormat.addInputPath(job, new Path(otherArgs[2]));
+            FileOutputFormat.setOutputPath(job, new Path(otherArgs[3]));
+
+            job.setInputFormatClass(TextInputFormat.class);
+            job.setOutputFormatClass(TextOutputFormat.class);
+
+            boolean succeded = job.waitForCompletion(true);
+
+            //If the job fails the application will be closed.
+            if(!succeded) {
+                System.err.println("Error in the job.");
+                System.exit(1);
+            }
+
+            // get new centroids
+            stop = KMeans.stoppingCriterion(oldCentroids, newCentroids, dist, t);
+        }
+
+        System.exit(0);
     }
 
 }
