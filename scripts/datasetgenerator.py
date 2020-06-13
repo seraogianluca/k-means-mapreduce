2c5a7462f86124c85864b3d02fad15b2258711a8
Gianluca Serao <gianluca@MBP-di-Gianluca.lan>
2020-06-13 11:51:59 +0200

    dataset creator script

--- scripts/datasetgenerator.py
+++ scripts/datasetgenerator.py
@@ -1,15 +1,37 @@
 from sklearn.datasets import make_blobs
 from matplotlib import pyplot
 from pandas import DataFrame
-# generate 2d classification dataset
-X, y = make_blobs(n_samples=3000, centers=5, n_features=3)
-# scatter plot, dots colored by class value
-df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
-colors = {0:'red', 1:'red', 2:'red', 3:'red', 4:'red', 5:'red', 6:'red'}
+
+dimension = 3
+samples = 1000
+centers = 40
+
+points, y = make_blobs(n_samples=samples, centers=centers, n_features=dimension)
+
+with open("dataset[1k,3].txt", "w") as file:
+    for point in points:
+        for value in range(dimension):
+            if value == (dimension - 1):
+                file.write(str(point[value]))
+            else:
+                file.write(str(point[value]) + ",")
+        file.write("\n")
+
+df = DataFrame(dict(x=points[:,0], y=points[:,1], label=y))
 fig, ax = pyplot.subplots()
 grouped = df.groupby('label')
+
 for key, group in grouped:
-    group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color = colors[key])
+    group.plot(ax=ax, kind='scatter', x='x', y='y')
 pyplot.show()
 
+"""
+fig, axs = pyplot.subplots(2,2)
+axs[0, 0].scatter(X[0], X[1])
+axs[1, 0].scatter(X[2], X[3])
+axs[0, 1].scatter(X[4], X[5])
+axs[1, 1].scatter(X[5], X[6])
+
+pyplot.show()
+"""
 
