f9cd27b0a856d3f24437cfd372cc5da728455615
thorongil <officialantonio@outlook.it>
2020-06-26 18:27:43 +0200

    add config.json add the saveAsTextFile
    Co-authored-by: ragnar1002 <lorenzobarigliano@gmail.com>

--- k-means-spark/spark.py
+++ k-means-spark/spark.py
@@ -1,14 +1,13 @@
 import sys
 import os
+import json
 import numpy as ny
 from pyspark import SparkContext
 from point import Point
+import time
 
-# os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
-# os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
-
-# centroids_broadcast
-# distance_broadcast
+#os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
+#os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
 
 def init_centroids(dataset, dataset_size, k):
     positions = ny.random.choice(range(dataset_size), size=k, replace=False)
@@ -42,13 +41,13 @@ def assign_centroids(row):
         if(distance < min_dist):
             min_dist = distance
             nearest_centroid = i
-    return nearest_centroid, p
+    return (nearest_centroid, p)
 
-def stopping_criterion(old, dist, threshold):
+def stopping_criterion(new_centroids, dist, threshold):
     check = True
     old_centroids = centroids_broadcast.value
     for i in range(len(old_centroids)):
-        check = old_centroids[i].distance(result[i][1], dist) < threshold
+        check = old_centroids[i].distance(new_centroids[i], dist) < threshold
         if check == False:
             return False
     return True
@@ -58,46 +57,45 @@ def reduce(x, y):
     return x
 
 if __name__ == "__main__":
-    sc = SparkContext("yarn", "Kmeans")
+    start_time = time.time()
+    sc = SparkContext("local", "Kmeans")
     print("\n***START****\n")
     sc.setLogLevel("ERROR")
     sc.addPyFile("./point.py") ## It's necessary, otherwise the spark framework doesn't see point.py
+    config = open('./config.json')
+    parameters = json.load(config)["configuration"][0]
+    config.close()    
     print("Parameters:", str(len(sys.argv)))
-    if len(sys.argv) < 9:
+    if len(sys.argv) < 2:
         print("Number of arguments not valid!")
         sys.exit(1)
-    input_path = str(sys.argv[1])
-    output_path = str(sys.argv[2]) if len(sys.argv) > 1 else "./output.txt"
-    dataset_size = int(sys.argv[3])
-    distance = int(sys.argv[4])
-    dim = int(sys.argv[5])
-    k = int(sys.argv[6])
-    treshold = float(sys.argv[7])
-    max_iterations = float(sys.argv[8])
+    INPUT_PATH = str(sys.argv[1])
+    OUTPUT_PATH = str(sys.argv[2]) if len(sys.argv) > 1 else "./output.txt"
+    DISTANCE_TYPE = parameters["distance"]
+    THRESHOLD = parameters["threshold"]
+    MAX_ITERATIONS = parameters["maxiteration"]
 
-    input_file = sc.textFile(input_path)
-    initial_centroids = init_centroids(input_file, dataset_size=dataset_size, k=k)
-    distance_broadcast = sc.broadcast(distance)
+    input_file = sc.textFile(INPUT_PATH)
+    initial_centroids = init_centroids(input_file, dataset_size=parameters["datasetsize"], k=parameters["k"])
+    distance_broadcast = sc.broadcast(DISTANCE_TYPE)
     centroids_broadcast = sc.broadcast(initial_centroids)
-    print("Iteration", 0)
-    print("Centroid broadcast:", centroids_broadcast.value)
-    stop = False
-    n = 0
-    while stop == False and n < max_iterations:
-        print("Iteration", n+1)
+    stop, n = False, 0
+    while stop == False and n < MAX_ITERATIONS:
         result = []
         map = input_file.map(lambda row: assign_centroids(row))
         sumRDD = map.reduceByKey(lambda x, y: reduce(x,y)) ## f(x) must be associative
         centroidsRDD = sumRDD.mapValues(lambda x: x.get_average_point())
-        result = centroidsRDD.collect()
-        stop = stopping_criterion(result, distance,treshold)
+        new_centroids = [item[1] for item in centroidsRDD.collect()]
+        stop = stopping_criterion(new_centroids, DISTANCE_TYPE,THRESHOLD)
         n += 1
-        if(stop == False and n < max_iterations):
-            centroids_broadcast.unpersist() 
-            centroids_broadcast.destroy()
-            centroids_broadcast = sc.broadcast([tpl[1] for tpl in result])
-    print("Iterations:", n)
-    print("\n***Centroids***\n")
-    #centroidsRDD.saveAsTextFile(output_path)
-    for tpl in centroidsRDD.collect():
-        print(tpl[1])
+        if(stop == False and n < MAX_ITERATIONS):
+            centroids_broadcast.unpersist()
+            centroids_broadcast = sc.broadcast(new_centroids)
+    fs = sc._jvm.org.apache.hadoop.fs.FileSystem.get(sc._jsc.hadoopConfiguration())
+    already_exists = fs.exists(sc._jvm.org.apache.hadoop.fs.Path(OUTPUT_PATH + "/_SUCCESS"))
+    if already_exists:
+        fs.delete(sc._jvm.org.apache.hadoop.fs.Path(OUTPUT_PATH), True)
+        print("File already exists, it has been overwritten")
+    centroidsRDD.saveAsTextFile(OUTPUT_PATH)
+    print("\nIterations:", n)
+    print("Time:", (time.time() - start_time), "s")
