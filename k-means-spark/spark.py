6a2c0753ca14344bdb1c8f6bc5b840f170c94ce6
Gianluca Serao <gianluca@MBP-di-Gianluca.lan>
2020-07-07 11:29:49 +0200

    spark final
    Co-authored-by: ragnar1002 <lorenzobarigliano@gmail.com>
    Co-authored-by: matildao-pane <edlitam@hotmail.it>
    Co-authored-by: thorongil05 <officialantonio@outlook.it>

--- k-means-spark/spark.py
+++ k-means-spark/spark.py
@@ -6,17 +6,16 @@ from pyspark import SparkContext
 from point import Point
 import time
 
-os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
-os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
+# os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
+# os.environ['PYSPARK_PYTHON'] = '/usr/local/bin/python3' ## TODO: Remove
 
 def init_centroids(dataset, k):
     start_time = time.time()
     initial_centroids = dataset.takeSample(False, k)
-    print("Centroids initialization:", len(initial_centroids), "in", (time.time() - start_time), "s")
+    print("init centroid execution:", len(initial_centroids), "in", (time.time() - start_time), "s")
     return initial_centroids
 
 def assign_centroids(p):
-    # Assign to the closest centroid
     min_dist = float("inf")
     centroids = centroids_broadcast.value
     nearest_centroid = 0
@@ -28,10 +27,9 @@ def assign_centroids(p):
     return (nearest_centroid, p)
 
 def stopping_criterion(new_centroids, threshold):
-    # O(dim *  k)
     old_centroids = centroids_broadcast.value
     for i in range(len(old_centroids)):
-        check = old_centroids[i].distance(new_centroids[i], distance_broadcast.value) < threshold
+        check = old_centroids[i].distance(new_centroids[i], distance_broadcast.value) <= threshold
         if check == False:
             return False
     return True
@@ -41,15 +39,18 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Number of arguments not valid!")
         sys.exit(1)
+
     with open('./config.json') as config:
         parameters = json.load(config)["configuration"][0]
+
     INPUT_PATH = str(sys.argv[1])
     OUTPUT_PATH = str(sys.argv[2]) if len(sys.argv) > 1 else "./output.txt"
+    
     sc = SparkContext("local", "Kmeans")
-    print("\n***START****\n")
-
     sc.setLogLevel("ERROR")
     sc.addPyFile("./point.py") ## It's necessary, otherwise the spark framework doesn't see point.py
+
+    print("\n***START****\n")
 
     points = sc.textFile(INPUT_PATH).map(Point).cache()
     initial_centroids = init_centroids(points, k=parameters["k"])
@@ -57,7 +58,6 @@ if __name__ == "__main__":
     centroids_broadcast = sc.broadcast(initial_centroids)
 
     stop, n = False, 0
-    stages_time = time.time()
     while True:
         print("--Iteration n. {itr:d}".format(itr=n+1), end="\r", flush=True)
         cluster_assignment_rdd = points.map(assign_centroids)
@@ -66,20 +66,18 @@ if __name__ == "__main__":
 
         new_centroids = [item[1] for item in centroids_rdd.collect()]
         stop = stopping_criterion(new_centroids,parameters["threshold"])
+
         n += 1
         if(stop == False and n < parameters["maxiteration"]):
-            centroids_broadcast.unpersist()
             centroids_broadcast = sc.broadcast(new_centroids)
         else:
             break
 
-    stages_time = time.time() - stages_time
     with open(OUTPUT_PATH, "w") as f:
         for centroid in new_centroids:
             f.write(str(centroid) + "\n")
 
-    print("\nIterations:", n)
-    print("Stages time:", stages_time, "s\n")
-    print("Average stage time:", stages_time/n, "s\n")
-    print("Total time:", (time.time() - start_time), "s")
-    print("Overhead time:", (time.time() - start_time) - stages_time, "s\n")
+    execution_time = time.time() - start_time
+    print("\nexecution time:", execution_time, "s")
+    print("average time per iteration:", execution_time/n, "s")
+    print("n_iter:", n)
